7dace284c3c7ddf0a31f5fd573766b2c9b0968c0
Josh Deprez <DrJosh9000@users.noreply.github.com>
2021-07-14 16:26:47 +1000

    debug print

--- game.go
+++ game.go
@@ -1,32 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const screenWidth, screenHeight = 320, 240
 
 type game struct{}
 
-// Update proceeds the game state.
-// Update is called every tick (1/60 [s] by default).
 func (g *game) Update() error {
 	// TODO
 	return nil
 }
 
-// Draw draws the game screen.
-// Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *game) Draw(screen *ebiten.Image) {
-	// TODO
-	screen.Fill(color.White)
+	screen.Fill(color.RGBA{50, 50, 50, 255})
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
 }
 
-// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-// If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *game) Layout(outsideWidth, outsideHeight int) (w, h int) {
 	return screenWidth, screenHeight
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module drjosh.dev/gurgle
 
 go 1.16
 
-require github.com/hajimehoshi/ebiten/v2 v2.1.3
+require (
+	github.com/hajimehoshi/ebiten v1.12.12
+	github.com/hajimehoshi/ebiten/v2 v2.1.3
+)
