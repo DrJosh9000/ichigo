293b013db415c3a1d2042cdf66ef4fa34230c568
Josh Deprez <DrJosh9000@users.noreply.github.com>
2021-07-25 14:15:39 +1000

    Implement tilemap

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"embed"
+	"image"
+	_ "image/png"
 	"log"
 
 	"drjosh.dev/gurgle/engine"
@@ -9,13 +12,48 @@ import (
 
 const screenWidth, screenHeight = 320, 240
 
+//go:embed assets/*
+var assets embed.FS
+
 func main() {
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
-	ebiten.SetWindowTitle("ebiten")
+	ebiten.SetWindowTitle("TODO")
+
+	boxesFile, err := assets.Open("assets/boxes.png")
+	if err != nil {
+		log.Fatalf("Couldn't open asset: %v", err)
+	}
+	boxesPNG, _, err := image.Decode(boxesFile)
+	if err != nil {
+		log.Fatalf("Couldn't decode asset: %v", err)
+	}
+	tiles := &engine.Tilemap{
+		Map: [][]int{
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+			{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+		},
+		Src:      ebiten.NewImageFromImage(boxesPNG),
+		TileSize: 16,
+	}
+
 	if err := ebiten.RunGame(&engine.Game{
 		ScreenHeight: screenHeight,
 		ScreenWidth:  screenWidth,
 		Components: []interface{}{
+			tiles,
 			engine.TPSDisplay{},
 		},
 	}); err != nil {
