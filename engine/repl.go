a19cdfdf6fb756e812ba9e894f82678f4a9003c2
Josh Deprez <josh.deprez@gmail.com>
2021-08-27 13:08:04 +1000

    refactor repl

--- engine/repl.go
+++ engine/repl.go
@@ -15,11 +15,11 @@ func (g *Game) REPL(src io.Reader, dst io.Writer, assets fs.FS) error {
 	fmt.Fprint(dst, "game>")
 	sc := bufio.NewScanner(src)
 	for sc.Scan() {
-		tok := strings.Split(sc.Text(), " ")
-		if len(tok) == 0 {
+		argv := strings.Split(sc.Text(), " ")
+		if len(argv) == 0 {
 			continue
 		}
-		switch tok[0] {
+		switch argv[0] {
 		case "quit":
 			os.Exit(0)
 		case "pause":
@@ -27,78 +27,80 @@ func (g *Game) REPL(src io.Reader, dst io.Writer, assets fs.FS) error {
 		case "resume", "unpause":
 			g.Enable()
 		case "save":
-			if len(tok) != 2 {
-				fmt.Fprintln(dst, "Usage: save ID")
-				break
-			}
-			id := tok[1]
-			c := g.Component(id)
-			if c == nil {
-				fmt.Fprintf(dst, "Component %q not found\n", id)
-				break
-			}
-			s, ok := c.(Saver)
-			if !ok {
-				fmt.Fprintf(dst, "Component %q not a Saver (type %T)\n", id, c)
-				break
-			}
-			if err := s.Save(); err != nil {
-				fmt.Fprintf(dst, "Couldn't save %q: %v\n", id, err)
-			}
+			g.cmdSave(dst, argv)
 		case "reload":
-			g.Disable()
-			g.Hide()
-			if err := g.Load(assets); err != nil {
-				fmt.Fprintf(dst, "Couldn't load: %v\n", err)
-				break
-			}
-			g.Prepare()
-			g.Enable()
-			g.Show()
+			g.cmdReload(dst, assets)
 		case "tree":
-			if len(tok) < 1 || len(tok) > 2 {
-				fmt.Println(dst, "Usage: tree [ID]")
-			}
-			var c interface{} = g
-			if len(tok) == 2 {
-				// subtree
-				id := tok[1]
-				c = g.Component(id)
-				if c == nil {
-					fmt.Fprintf(dst, "Component %q not found\n", id)
-					break
-				}
-			}
-			type item struct {
-				c     interface{}
-				depth int
-			}
-			stack := []item{{c, 0}}
-			for len(stack) > 0 {
-				tail := len(stack) - 1
-				x := stack[tail]
-				stack = stack[:tail]
-				c := x.c
-
-				indent := ""
-				if x.depth > 0 {
-					indent = strings.Repeat("  ", x.depth-1) + "↳ "
-				}
-				i, ok := c.(Identifier)
-				if ok {
-					fmt.Fprintf(dst, "%s%T %s\n", indent, c, i.Ident())
-				} else {
-					fmt.Fprintf(dst, "%s%T\n", indent, c)
-				}
-
-				if s, ok := c.(Scanner); ok {
-					for _, y := range s.Scan() {
-						stack = append(stack, item{y, x.depth + 1})
-					}
-				}
-			}
+			g.cmdTree(dst, argv)
 		}
 		fmt.Fprint(dst, "game>")
 	}
 	return sc.Err()
+}
+
+func (g *Game) cmdSave(dst io.Writer, argv []string) {
+	if len(argv) != 2 {
+		fmt.Fprintln(dst, "Usage: save ID")
+		return
+	}
+	id := argv[1]
+	c := g.Component(id)
+	if c == nil {
+		fmt.Fprintf(dst, "Component %q not found\n", id)
+		return
+	}
+	s, ok := c.(Saver)
+	if !ok {
+		fmt.Fprintf(dst, "Component %q not a Saver (type %T)\n", id, c)
+		return
+	}
+	if err := s.Save(); err != nil {
+		fmt.Fprintf(dst, "Couldn't save %q: %v\n", id, err)
+	}
+}
+
+func (g *Game) cmdReload(dst io.Writer, assets fs.FS) {
+	g.Disable()
+	g.Hide()
+	if err := g.Load(assets); err != nil {
+		fmt.Fprintf(dst, "Couldn't load: %v\n", err)
+		return
+	}
+	g.Prepare()
+	g.Enable()
+	g.Show()
+}
+
+func (g *Game) cmdTree(dst io.Writer, argv []string) {
+	if len(argv) < 1 || len(argv) > 2 {
+		fmt.Println(dst, "Usage: tree [ID]")
+	}
+	var c interface{} = g
+	if len(argv) == 2 { // subtree
+		id := argv[1]
+		c = g.Component(id)
+		if c == nil {
+			fmt.Fprintf(dst, "Component %q not found\n", id)
+			return
+		}
+	}
+	var walk func(interface{}, int)
+	walk = func(c interface{}, depth int) {
+		indent := ""
+		if depth > 0 {
+			indent = strings.Repeat("  ", depth-1) + "↳ "
+		}
+		i, ok := c.(Identifier)
+		if ok {
+			fmt.Fprintf(dst, "%s%T %s\n", indent, c, i.Ident())
+		} else {
+			fmt.Fprintf(dst, "%s%T\n", indent, c)
+		}
+		if s, ok := c.(Scanner); ok {
+			for _, d := range s.Scan() {
+				walk(d, depth+1)
+			}
+		}
+	}
+	walk(c, 0)
 }
